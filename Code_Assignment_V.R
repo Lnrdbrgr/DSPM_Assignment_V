7ea79d4e568f76a38cf879cd309cbcf9e4e3ecc3
Leonard Berger <leonard.berger@student.uni-tuebingen.de>
2021-02-03 08:36:03 +0100

    Set up Preparation (packages, working directory, API Key)

--- Code_Assignment_V.R
+++ Code_Assignment_V.R
@@ -5,6 +5,28 @@
 
 
 
+#### Exercise 2 - Gettin to know the API
+
+# Allowed: 5000 API calls per day and 5 requests per second
+
+
+## Installing packages
+# install.packages("jsonlite")
+# install.packages("httr")
+library(jsonlite)
+library(httr)
+
+
+
+# set working directory
+setwd("C:/Users/leona/OneDrive/Data Science Project Management/DSPM_Assignment_V")
+
+
+# include API Key
+source("C:/Users/leona/OneDrive/Uni Tübingen Data Science/1. Semester/Data Science Project Management/Assignment 5/API_key.R")
+
+
+
 
 
 
