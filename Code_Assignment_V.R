08a973898f9b0eac480a98bfd916703799c06291
Leonard Berger <leonard.berger@student.uni-tuebingen.de>
2021-02-05 10:25:42 +0100

    Now: Final Doc

--- Code_Assignment_V.R
+++ Code_Assignment_V.R
@@ -223,6 +223,186 @@ venue_data_2$latitude_mod <- ifelse((venue_data_2$latitude < 47.271679 | venue_d
 
 
 
+#### Visualization of event locations in Germany
+
+
+## set up a map of germany which diplays the locations of the venues
+ggplot() +
+  geom_polygon(
+    aes(x = long, y = lat, group = group), data = map_data("world", region = "Spain"),
+    fill = "grey90",color = "black") +
+  theme_void() + coord_quickmap() +
+  labs(title = "Event locations across Germany", caption = "Source: ticketmaster.com") +
+  theme(title = element_text(size=8, face='bold'),
+        plot.caption = element_text(face = "italic")) +
+  geom_point(aes(x = longitude_mod,
+                 y = latitude_mod),
+             data = venue_data_2,
+             alpha = 05,
+             size = 0.7,
+             col = "red")
+
+
+
+## Set up the Same for Spain
+
+
+# Perform the GET request examplary for the first page
+APIcontent <- GET(url = api_url,
+                  query = list(apikey = API_key,
+                               locale = "*",
+                               countryCode = "ES"))
+
+
+APIcontent_Spain <- GET(url = api_url,
+                        query = list(apikey = API_key,
+                                     locale = "*",
+                                     countryCode = "ES"))
+
+
+venue_results_Spain <- content(APIcontent_Spain)
+
+
+# extract the total number of Pages
+no_pages_Spain <- venue_results_Spain$page$totalPages
+
+
+
+### Extract all locations
+
+## set up Data frame
+venue_data_Spain <- data.frame("name" = NA,
+                               "city" = NA,
+                               "postalCode" = NA,
+                               "address" = NA,
+                               "url" = NA,
+                               "longitude" = NA,
+                               "latitude" = NA)
+
+
+## loop through all pages and extract locations
+
+for (i in 0:no_pages_Spain){
+  
+  # GET request
+  APIcontent_Spain <- GET(url = api_url,
+                          query = list(apikey = API_key,
+                                       countryCode = "ES",
+                                       locale="*",
+                                       page = i))
+  
+  # get content
+  venue_results_Spain <- content(APIcontent_Spain)
+  
+  # help data frame to store results
+  venue_help_df_Spain <- data.frame("name" = rep(NA, 20),
+                                    "city" = rep(NA, 20),
+                                    "postalCode" = rep(NA, 20),
+                                    "address" = rep(NA, 20),
+                                    "url" = rep(NA, 20),
+                                    "longitude" = rep(NA, 20),
+                                    "latitude" = rep(NA, 20)) 
+  
+  
+  # extract the data from the list
+  for (j in 1:20){
+    
+    
+    # account for missing values in latitude and longitude
+    if (!is.null(venue_results_Spain$`_embedded`[[1]][[j]]$name)){
+      venue_help_df_Spain$name[j] <- venue_results_Spain$`_embedded`[[1]][[j]]$name
+    }
+    
+    if (!is.null(venue_results_Spain$`_embedded`[[1]][[j]]$city)){
+      venue_help_df_Spain$city[j] <- venue_results_Spain$`_embedded`[[1]][[j]]$city
+    }
+    
+    if (!is.null(venue_results_Spain$`_embedded`[[1]][[j]]$postalCode)){
+      venue_help_df_Spain$postalCode[j] <- venue_results_Spain$`_embedded`[[1]][[j]]$postalCode
+    }
+    
+    
+    if(length(venue_results_Spain$`_embedded`[[1]][[j]]$address)==0){
+      venue_help_df_Spain$address[j] <- NA
+    } else if (!is.null(venue_results_Spain$`_embedded`[[1]][[j]]$address)){
+      venue_help_df_Spain$address[j] <- venue_results_Spain$`_embedded`[[1]][[j]]$address
+    }
+    
+    
+    if (!is.null(venue_results_Spain$`_embedded`[[1]][[j]]$url)){
+      venue_help_df_Spain$url[j] <- venue_results_Spain$`_embedded`[[1]][[j]]$url
+    }
+    
+    
+    if (!is.null(venue_results_Spain$`_embedded`[[1]][[j]]$location$longitude)){
+      venue_help_df_Spain$longitude[j] <- venue_results_Spain$`_embedded`[[1]][[j]]$location$longitude
+    }
+    
+    if (!is.null(venue_results_Spain$`_embedded`[[1]][[j]]$location$latitude)){
+      venue_help_df_Spain$latitude[j] <- venue_results_Spain$`_embedded`[[1]][[j]]$location$latitude
+    }
+    
+  }
+  
+  # perform some modification on the data types
+  venue_help_df_Spain$city <- as.character(venue_help_df_Spain$city)
+  venue_help_df_Spain$address <- as.character(venue_help_df_Spain$address)
+  venue_help_df_Spain$longitude <- as.numeric(venue_help_df_Spain$longitude)
+  venue_help_df_Spain$latitude <- as.numeric(venue_help_df_Spain$latitude)
+  
+  
+  # ignore efficiency for the time being and append the data frame
+  venue_data_Spain <- rbind(venue_data_Spain, venue_help_df_Spain)
+  
+  
+  # sleep for X seconds to stay in line with the guidelines
+  Sys.sleep(0.33)
+  
+}
+
+
+
+write.csv(venue_data_Spain, "venue_data_spain.csv")
+
+
+## make some modifications to the resulting data frame
+venue_data_Spain_2 <- venue_data_Spain[-1,] 
+
+glimpse(venue_data_Spain_2)
+
+
+# adjust lat and long to extreme points of Spain
+venue_data_Spain_2$longitude_mod <- ifelse((venue_data_Spain_2$longitude < -9.3 | venue_data_Spain_2$longitude > 3.316667), NA, venue_data_Spain_2$longitude)
+venue_data_Spain_2$latitude_mod <- ifelse((venue_data_Spain_2$latitude < 36 | venue_data_Spain_2$latitude > 43.783333), NA, venue_data_Spain_2$latitude)
+
+
+
+## set up a map of Spain which diplays the locations of the venues
+ggplot() +
+  geom_polygon(
+    aes(x = long, y = lat, group = group), data = map_data("world", region = "Spain"),
+    fill = "grey90",color = "black") +
+  theme_void() + coord_quickmap() +
+  labs(title = "Event locations across Spain", caption = "Source: ticketmaster.com") +
+  theme(title = element_text(size=8, face='bold'),
+        plot.caption = element_text(face = "italic")) +
+  geom_point(aes(x = longitude_mod,
+                 y = latitude_mod),
+             data = venue_data_Spain_2,
+             alpha = 0.5,
+             size = 0.7,
+             col = "red")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
